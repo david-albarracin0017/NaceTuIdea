8ea702cdbb5f1b2d84a236ffb22cf268687daebb
dina_ <dina_@DINOSAURIO>
2025-05-12 22:29:41 -0500

    cc

--- Backend/Controllers/AccessController.cs
+++ Backend/Controllers/AccessController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Backend.Context;
 using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Crypto.Generators;
 
 namespace Backend.Controllers
 {
